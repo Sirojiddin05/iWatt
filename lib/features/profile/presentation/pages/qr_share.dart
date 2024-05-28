93954cf72e8efea262d86f26ab7eec15bd2c3559
s.rajabboyev <s.rajabboyev@uic.group>
2024-05-28 14:25:22 +0500

    qr_share ui done!

--- lib/features/profile/presentation/pages/qr_share.dart
+++ lib/features/profile/presentation/pages/qr_share.dart
@@ -1,5 +1,6 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:i_watt_app/core/config/app_images.dart';
 import 'package:i_watt_app/features/common/presentation/widgets/app_bar_wrapper.dart';
 import 'package:i_watt_app/generated/locale_keys.g.dart';
 
@@ -17,6 +18,61 @@ class _QrSharePageState extends State<QrSharePage> {
       appBar: AppBarWrapper(
         hasBackButton: true,
         title: LocaleKeys.login_with_qr.tr(),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: MediaQuery.of(context).size.width * .66,
+              height: MediaQuery.of(context).size.width * .66,
+              padding: const EdgeInsets.all(20),
+              clipBehavior: Clip.antiAlias,
+              decoration: ShapeDecoration(
+                color: Colors.white,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                shadows: const [
+                  BoxShadow(
+                    color: Color(0x0C000000),
+                    blurRadius: 21.10,
+                    offset: Offset(0, 4),
+                    spreadRadius: 0,
+                  ),
+                  BoxShadow(
+                    color: Color(0x0C000000),
+                    blurRadius: 7.90,
+                    offset: Offset(0, 1),
+                    spreadRadius: 1,
+                  ),
+                  BoxShadow(
+                    color: Color(0x0C000000),
+                    blurRadius: 6.30,
+                    offset: Offset(0, 0),
+                    spreadRadius: 1,
+                  )
+                ],
+              ),
+              child: Image.asset(AppImages.shareQrExample),
+            ),
+            const SizedBox(height: 24),
+            Text(
+              "Использовать пароль с другого устройства?",
+              textAlign: TextAlign.center,
+              style: Theme.of(context).textTheme.displayLarge,
+            ),
+            const SizedBox(height: 6),
+            Text(
+              "Отсканируйте этот QR-код с помощью устройства, на котором есть пароль, который вы хотите использовать для входа I-Watt",
+              textAlign: TextAlign.center,
+              style: Theme.of(context).textTheme.labelMedium?.copyWith(
+                    color: Theme.of(context).bottomNavigationBarTheme.unselectedLabelStyle?.color,
+                  ),
+            ),
+          ],
+        ),
       ),
     );
   }
